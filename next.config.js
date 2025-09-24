713048c3f0d55d1365820f5648795157e70eb525
GetBrain <daniel@getbrain.com.br>
2025-09-24 13:54:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
-  // Se usa imagens externas, configure domains aqui:
   // images: { domains: ['...'] },
 };
 module.exports = nextConfig;
